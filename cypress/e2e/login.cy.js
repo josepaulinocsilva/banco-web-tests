4834f8b3cffca0d6648bd454d4b70c4d27fb10aa
josepaulinocsilva <jpaulino57@hotmail.com>
2026-07-27 19:39:09 -0300

    Adicionando scripts para execução dos testes no package.json

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,8 @@
 describe("Login", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:4000");
-  });
-
   it("Login com dados válidos, deve permitir entrada no sistema", () => {
+    // arrange
+    cy.visit("http://localhost:4000");
+
     // act
     cy.get("#username").type("julio.lima");
     cy.get("#senha").type("123456");
@@ -14,6 +13,9 @@ describe("Login", () => {
   });
 
   it("Login com dados inválidos, deve apresentar mensagem de erro entrada no sistema", () => {
+    // arrange
+    cy.visit("http://localhost:4000");
+
     // act
     cy.get("#username").type("julio.lima");
     cy.get("#senha").type("1234");
