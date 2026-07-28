6b33f1ef63b3348941122348c81ca19160a93251
josepaulinocsilva <jpaulino57@hotmail.com>
2026-07-28 14:38:22 -0300

    Adicionando screenshots e video

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    video: false, // desabilita a gravação de vídeo dos testes
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
