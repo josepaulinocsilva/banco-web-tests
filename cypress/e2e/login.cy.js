4eba19bd9e9f8fafe849ba9155d7f32a52db72d6
josepaulinocsilva <jpaulino57@hotmail.com>
2026-07-31 14:37:58 -0300

    Implementando comandos customizaveis para login, transferencias e commons

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,37 +1,23 @@
 describe("Login", () => {
   beforeEach(() => {
+    // Arrange
     cy.visit("/");
     cy.screenshot("Após carregar a página de login");
   });
 
   it("Login com dados válidos, deve permitir entrada no sistema", () => {
-    // act
-    cy.fixture("credenciais").then((credenciais) => {
-      cy.get("#username").type(credenciais.valida.usuario);
-      cy.get("#senha").type(credenciais.valida.senha);
-    });
+    // Act
+    cy.fazerLoginComCredenciaisValidas();
 
-    cy.screenshot("Após carregar a página de login e seus dados");
-    cy.contains("button", "Entrar").click();
-    cy.screenshot("Após clicar no botão de entrar");
-
-    // assert
+    // Assert
     cy.contains("h4", "Realizar Transferência").should("be.visible");
   });
 
   it("Login com dados inválidos, deve apresentar mensagem de erro entrada no sistema", () => {
-    // act
-    cy.fixture("credenciais").then((credenciais) => {
-      cy.get("#username").type(credenciais.invalida.usuario);
-      cy.get("#senha").type(credenciais.invalida.senha);
-    });
+    // Act
+    cy.fazerLoginComCredenciaisInvalidas();
 
-    //cy.get("#login-section > .btn").click(); pode ser desta forma ou a forma abaixo mais legível
-    cy.contains("button", "Entrar").click();
-
-    // assert
-    //cy.get(".toast").should("be.visible");
-
-    cy.get(".toast").should("have.text", "Erro no login. Tente novamente.");
+    // Assert
+    cy.verificarMensagemNoToast("Erro no login. Tente novamente.");
   });
 });
--- cypress/e2e/transferencia.cy.js
+++ cypress/e2e/transferencia.cy.js
@@ -1,28 +1,24 @@
 describe("Transferencia", () => {
   beforeEach(() => {
     cy.visit("/");
-    cy.screenshot("Após carregar a página de login");
-
-    cy.fixture("credenciais").then((credenciais) => {
-      cy.get("#username").type(credenciais.valida.usuario);
-      cy.get("#senha").type(credenciais.valida.senha);
-    });
-    cy.contains("button", "Entrar").click();
+    cy.fazerLoginComCredenciaisValidas();
   });
 
   it("Deve transferir quando informo dados e valor validos", () => {
-    cy.get('label[for="conta-origem"]').parent().as("campo-conta-origem");
-    cy.get("@campo-conta-origem").click();
-    cy.get("@campo-conta-origem").contains("João da Silva com saldo").click();
+    // Act
+    cy.realizarTransferencia ("João", "Paulino", "15.50"),
 
-    cy.get('label[for="conta-destino"]').parent().as("campo-conta-destino");
-    cy.get("@campo-conta-destino").click();
-    cy.get("@campo-conta-destino").contains("Paulino com saldo").click();
+    // Assert
+    cy.verificarMensagemNoToast("Transferência realizada!");
+  });
 
-    cy.get("#valor").click().type("37.35");
+  it("Deve apresentar erro quando tentar transferir mais que 5 mil sem o token", () => {
+    // Act
+    cy.realizarTransferencia ("João", "Paulino", "6000"),
 
-    cy.contains("button", "Transferir").click();
-
-    cy.get(".toast").should("have.text", "Transferência realizada!");
+    // Assert
+    cy.verificarMensagemNoToast("Autenticação necessária para transferências acima de R$5.000,00.");
   });
 });
+
+//   cy.get(".toast").should("have.text", "Transferência realizada!");
